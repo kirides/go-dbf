3c6c5d6b226c75a77dfac65cd125034fc4c44e82
Kirides <john.heckendorf@outlook.com>
2020-03-05 20:39:30 +0100

    fix memo-fields (broken in previous commit), ...
    extracted logic for parsing Fields to
    `(*Record).parseField(*Field) (interface{}, bool, error)`

--- record.go
+++ record.go
@@ -24,6 +24,7 @@ type Record struct {
 	parseOptions  ParseOption
 
 	nullFlags uint64
+	intBuf    [4]byte
 }
 
 // Deleted returns a bool that tells if a record is marked as deleted or not
@@ -57,13 +58,10 @@ func (r *Record) parse() {
 
 // ToMap parses the record into a map[string]interface{}
 func (r *Record) ToMap() (map[string]interface{}, error) {
-	var err error
 	if !r.read {
 		r.parse()
 	}
 	m := make(map[string]interface{})
-	trimRight := (r.parseOptions & ParseTrimRight) != 0
-	intBuf := make([]byte, 4, 4)
 
 	for _, f := range r.dbf.fields {
 		// Skip internal columns
@@ -77,60 +75,12 @@ func (r *Record) ToMap() (map[string]interface{}, error) {
 			}
 		}
 
-		switch f.Type {
-		case 'I':
-			m[f.Name] = binary.LittleEndian.Uint32(r.buffer[f.Displacement:])
-		case 'C':
-			v, _ := r.dbf.decoder.Bytes(r.buffer[f.Displacement : f.Displacement+uint32(f.Length)])
-			if trimRight {
-				v = bytes.TrimRight(v, " ")
-			}
-			m[f.Name] = string(v)
-		case 'D':
-			m[f.Name], _ = time.Parse("20060102", string(r.buffer[f.Displacement:f.Displacement+uint32(f.Length)]))
-		case 'T':
-			m[f.Name] = julianDateTimeToTime(binary.LittleEndian.Uint64(r.buffer[f.Displacement:]))
-		case 'N':
-			if f.DecimalCount == 0 {
-				m[f.Name], _ = strconv.ParseInt(string(r.buffer[f.Displacement:f.Displacement+uint32(f.Length)]), 10, 64)
-			} else {
-				m[f.Name], _ = strconv.ParseFloat(string(r.buffer[f.Displacement:f.Displacement+uint32(f.Length)]), 64)
-			}
-		case 'L':
-			v := r.buffer[f.Displacement]
-			if v != 32 && v > 0 {
-				m[f.Name] = true
-			} else {
-				m[f.Name] = false
-			}
-		case 'M':
-			offset := binary.LittleEndian.Uint32(r.buffer[f.Displacement : f.Displacement+uint32(f.Length)])
-			if offset == 0 {
-				m[f.Name] = ""
-				continue
-			}
-			_, err = r.memoFile.Seek(4+int64(offset)*r.memoBlockSize, io.SeekStart)
-			if err != nil {
-				return nil, err
-			}
-			_, err = r.memoFile.Read(intBuf)
-			if err != nil {
-				return nil, err
-			}
-			memoSize := int(binary.BigEndian.Uint32(intBuf))
-			if memoSize == 0 {
-				m[f.Name] = ""
-				continue
-			}
-			if len(r.memoBuffer) < memoSize {
-				r.memoBuffer = make([]byte, memoSize, memoSize)
-			}
-			_, err = r.memoFile.Read(r.memoBuffer[:memoSize])
-			if err != nil {
-				return nil, err
-			}
-			v, _ := r.dbf.decoder.Bytes(r.buffer[f.Displacement : f.Displacement+uint32(f.Length)])
-			m[f.Name] = string(v)
+		v, ok, err := r.parseField(&f)
+		if err != nil {
+			return nil, err
+		}
+		if ok {
+			m[f.Name] = v
 		}
 	}
 
@@ -139,13 +89,10 @@ func (r *Record) ToMap() (map[string]interface{}, error) {
 
 // ToSlice parses the record into a []interface{}
 func (r *Record) ToSlice() ([]interface{}, error) {
-	var err error
 	if !r.read {
 		r.parse()
 	}
 	m := make([]interface{}, len(r.dbf.fields))
-	trimRight := (r.parseOptions & ParseTrimRight) != 0
-	intBuf := make([]byte, 4, 4)
 
 	for i, f := range r.dbf.fields {
 		// Skip internal columns
@@ -158,65 +105,76 @@ func (r *Record) ToSlice() ([]interface{}, error) {
 				continue
 			}
 		}
-
-		switch f.Type {
-		case 'I':
-			m[i] = binary.LittleEndian.Uint32(r.buffer[f.Displacement:])
-		case 'C':
-			v, _ := r.dbf.decoder.Bytes(r.buffer[f.Displacement : f.Displacement+uint32(f.Length)])
-			if trimRight {
-				v = bytes.TrimRight(v, " ")
-			}
-			m[i] = string(v)
-		case 'D':
-			m[i], _ = time.Parse("20060102", string(r.buffer[f.Displacement:f.Displacement+uint32(f.Length)]))
-		case 'T':
-			m[i] = julianDateTimeToTime(binary.LittleEndian.Uint64(r.buffer[f.Displacement:]))
-		case 'N':
-			if f.DecimalCount == 0 {
-				m[i], _ = strconv.ParseInt(string(r.buffer[f.Displacement:f.Displacement+uint32(f.Length)]), 10, 64)
-			} else {
-				m[i], _ = strconv.ParseFloat(string(r.buffer[f.Displacement:f.Displacement+uint32(f.Length)]), 64)
-			}
-		case 'L':
-			v := r.buffer[f.Displacement]
-			if v != 32 && v > 0 {
-				m[i] = true
-			} else {
-				m[i] = false
-			}
-		case 'M':
-			offset := binary.LittleEndian.Uint32(r.buffer[f.Displacement : f.Displacement+uint32(f.Length)])
-			if offset == 0 {
-				m[i] = ""
-				continue
-			}
-			_, err = r.memoFile.Seek(4+int64(offset)*r.memoBlockSize, io.SeekStart)
-			if err != nil {
-				return nil, err
-			}
-			_, err = r.memoFile.Read(intBuf)
-			if err != nil {
-				return nil, err
-			}
-			memoSize := int(binary.BigEndian.Uint32(intBuf))
-			if memoSize == 0 {
-				m[i] = ""
-				continue
-			}
-			if len(r.memoBuffer) < memoSize {
-				r.memoBuffer = make([]byte, memoSize, memoSize)
-			}
-			_, err = r.memoFile.Read(r.memoBuffer[:memoSize])
-			if err != nil {
-				return nil, err
-			}
-			v, _ := r.dbf.decoder.Bytes(r.buffer[f.Displacement : f.Displacement+uint32(f.Length)])
-			m[i] = string(v)
+		v, ok, err := r.parseField(&f)
+		if err != nil {
+			return nil, err
+		}
+		if ok {
+			m[i] = v
 		}
 	}
 
 	return m, nil
+}
+
+func (r *Record) parseField(f *Field) (interface{}, bool, error) {
+
+	trimRight := (r.parseOptions & ParseTrimRight) != 0
+	switch f.Type {
+	case 'I':
+		return binary.LittleEndian.Uint32(r.buffer[f.Displacement:]), true, nil
+	case 'C':
+		v, _ := r.dbf.decoder.Bytes(r.buffer[f.Displacement : f.Displacement+uint32(f.Length)])
+		if trimRight {
+			v = bytes.TrimRight(v, " ")
+		}
+		return string(v), true, nil
+	case 'D':
+		v, _ := time.Parse("20060102", string(r.buffer[f.Displacement:f.Displacement+uint32(f.Length)]))
+		return v, true, nil
+	case 'T':
+		return julianDateTimeToTime(binary.LittleEndian.Uint64(r.buffer[f.Displacement:])), true, nil
+	case 'N':
+		if f.DecimalCount == 0 {
+			v, _ := strconv.ParseInt(string(r.buffer[f.Displacement:f.Displacement+uint32(f.Length)]), 10, 64)
+			return v, true, nil
+		}
+		v, _ := strconv.ParseFloat(string(r.buffer[f.Displacement:f.Displacement+uint32(f.Length)]), 64)
+		return v, true, nil
+	case 'L':
+		v := r.buffer[f.Displacement]
+		if v != 32 && v > 0 {
+			return true, true, nil
+		}
+		return false, true, nil
+	case 'M':
+		offset := binary.LittleEndian.Uint32(r.buffer[f.Displacement : f.Displacement+uint32(f.Length)])
+		if offset == 0 {
+			return "", true, nil
+		}
+		_, err := r.memoFile.Seek(4+int64(offset)*r.memoBlockSize, io.SeekStart)
+		if err != nil {
+			return nil, false, err
+		}
+		_, err = r.memoFile.Read(r.intBuf[:])
+		if err != nil {
+			return nil, false, err
+		}
+		memoSize := int(binary.BigEndian.Uint32(r.intBuf[:]))
+		if memoSize == 0 {
+			return "", true, nil
+		}
+		if len(r.memoBuffer) < memoSize {
+			r.memoBuffer = make([]byte, memoSize, memoSize)
+		}
+		_, err = r.memoFile.Read(r.memoBuffer[:memoSize])
+		if err != nil {
+			return nil, false, err
+		}
+		v, _ := r.dbf.decoder.Bytes(r.memoBuffer[:memoSize])
+		return string(v), true, nil
+	}
+	return nil, false, nil
 }
 
 var minimumDateTime = time.Date(0001, time.Month(1), 1, 0, 0, 0, 0, time.Local)
